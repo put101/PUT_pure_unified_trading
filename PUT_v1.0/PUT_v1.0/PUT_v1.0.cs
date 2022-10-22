58c38743991e918101582e3eeba199d571794079
put101 <tobiaspucher@gmail.com>
2022-10-22 18:37:36 +0200

    init this mf.
    init this mf.

---
+++ PUT_v1.0/PUT_v1.0/PUT_v1.0.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Robots
+{
+    [Robot(AccessRights = AccessRights.None)]
+    public class PUT_v10 : Robot
+    {
+        [Parameter(DefaultValue = "Hello world!")]
+        public string Message { get; set; }
+
+        protected override void OnStart()
+        {
+            // To learn more about cTrader Automate visit our Help Center:
+            // https://help.ctrader.com/ctrader-automate
+
+            Print(Message);
+        }
+
+        protected override void OnTick()
+        {
+            // Handle price updates here
+        }
+
+        protected override void OnStop()
+        {
+            // Handle cBot stop here
+        }
+    }
+}
